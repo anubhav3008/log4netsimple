bbe6c1b4cff5e6fe885754d4b4dc6bbf0aedc786
Anubhav Shrivastava <anubhav.workemail@gmail.com>
2023-01-27 15:23:47 +0530

    first commit

---
+++ demo/One_Basic.cs
@@ -0,0 +1,17 @@
+﻿using log4net.Config;
+using log4net;
+using System.Reflection;
+
+namespace demo
+{
+    public class One_Basic
+    {
+        static void Main(string[] args)
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            ILog logger = LogManager.GetLogger(typeof(One_Basic));
+            logger.Info("hello");
+        }
+    }
+}
